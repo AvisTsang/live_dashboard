bfac6068084095e6ea291e90130a19e18c0d64e7
Avis <tsangching824@gmail.com>
2026-07-04 15:59:00 +0800

    added PWA

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+self.addEventListener("install", () => {
+    console.log("SW Installed");
+});
+
+self.addEventListener("fetch", (event) => {
+});
+
+if ("serviceWorker" in navigator){
+
+    navigator.serviceWorker
+        .register("./service-worker.js")
+        .then(() => {
+            console.log("SW Ready");
+        });
+
+}
