6123aa82309488d73950beff293b2578c24bc941
Avis <tsangching824@gmail.com>
2026-07-04 17:34:07 +0800

    update display

--- service-worker.js
+++ service-worker.js
@@ -2,10 +2,3 @@ self.addEventListener("install", () => {
     console.log("SW Installed");
 });
 
-self.addEventListener("fetch", event => {
-
-    event.respondWith(
-        fetch(event.request)
-    );
-
-});
